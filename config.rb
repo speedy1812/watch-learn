66d6a1e705a4510e0eba8893040f445fb5353fc8
Nathan Day <adayinukraine@gmail.com>
2016-10-27 15:19:26 +0300

    Remove generated MM files and configure Gulp.

--- config.rb
+++ config.rb
@@ -29,11 +29,30 @@ page '/*.txt', layout: false
 #   end
 # end
 
-# Build-specific configuration
 configure :build do
-  # Minify CSS on build
-  # activate :minify_css
+  activate :external_pipeline,
+    name: :gulp,
+    command: "npm run production",
+    source: ".tmp",
+    latency: 1
 
-  # Minify Javascript on build
-  # activate :minify_javascript
+  activate :gzip
+
+  # activate :asset_hash
+  # activate :asset_host,
+  #   host: "//cdn.cloudfront.net",
+  #   rewrite_ignore: ["*.js"]
+
+  # set :url_root, "http://example.com"
+
+  # https://github.com/Aupajo/middleman-search_engine_sitemap
+  # activate :search_engine_sitemap
+
+  ignore "javascripts/all.js"
+  ignore "stylesheets/site"
+
+  activate :minify_html do |html|
+    html.remove_quotes = false
+    html.remove_intertag_spaces = true
+  end
 end
